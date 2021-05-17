b59c6a8193f7515f6611cb16eee7685dd54e9c10
gilzoide <gilzoide@gmail.com>
2021-05-17 19:37:04 -0300

    Change license from UNLICENSE to MPLv2

--- main.gd
+++ main.gd
@@ -1,3 +1,7 @@
+# Copyright (c) 2021 Gil Barbosa Reis.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 extends Control
 
 const UNDOREDO_ACTION_DRAW_ITEM = "draw_item"
